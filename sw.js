14c2e3bd44dcab05f2970cd2924806cc31ece1d4
lottiedotcom <notilottie@gmail.com>
2026-08-09 21:33:14 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-const CACHE = 'SanctuaryCacheV1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/sound.jpg'
-];
+const CACHE_NAME = 'sanctuary-v2';
 
-self.addEventListener('install', (event) => {
+window.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
